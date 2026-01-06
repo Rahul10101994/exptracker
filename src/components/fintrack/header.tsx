a27add1363c6bf51ad85ade105d895c83ba8aa11
Rahul Sharma <rahulsharmab94@gmail.com>
2026-01-06 15:21:02 +0000

    Try fixing this error: `Console Error: A tree hydrated but some attribut

--- src/components/fintrack/header.tsx
+++ src/components/fintrack/header.tsx
@@ -2,6 +2,7 @@
 "use client";
 
 import Link from "next/link";
+import * as React from "react";
 import { format } from "date-fns";
 import { MoreVertical, Settings } from "lucide-react";
 import { Button } from "@/components/ui/button";
@@ -14,9 +15,14 @@ import {
 import { useUser } from "@/firebase";
 
 export function FinTrackHeader() {
+  const [isMounted, setIsMounted] = React.useState(false);
   const currentDate = format(new Date(), "MMMM d");
   const userContext = useUser();
   const userName = userContext?.user?.displayName || 'User';
+
+  React.useEffect(() => {
+    setIsMounted(true);
+  }, []);
 
   return (
     <header
@@ -27,33 +33,37 @@ export function FinTrackHeader() {
       "
     >
       {/* Left: Menu */}
-      <DropdownMenu>
-        <DropdownMenuTrigger asChild>
-          <Button
-            variant="ghost"
-            size="icon"
-            className="h-8 w-8"
-          >
-            <MoreVertical className="h-5 w-5" />
-            <span className="sr-only">Menu</span>
-          </Button>
-        </DropdownMenuTrigger>
+      <div className="h-8 w-8">
+        {isMounted && (
+          <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+              <Button
+                variant="ghost"
+                size="icon"
+                className="h-8 w-8"
+              >
+                <MoreVertical className="h-5 w-5" />
+                <span className="sr-only">Menu</span>
+              </Button>
+            </DropdownMenuTrigger>
 
-        <DropdownMenuContent
-          align="start"
-          className="w-44"
-        >
-          <DropdownMenuItem asChild>
-            <Link
-              href="/settings"
-              className="flex items-center"
+            <DropdownMenuContent
+              align="start"
+              className="w-44"
             >
-              <Settings className="mr-2 h-4 w-4" />
-              <span>Settings</span>
-            </Link>
-          </DropdownMenuItem>
-        </DropdownMenuContent>
-      </DropdownMenu>
+              <DropdownMenuItem asChild>
+                <Link
+                  href="/settings"
+                  className="flex items-center"
+                >
+                  <Settings className="mr-2 h-4 w-4" />
+                  <span>Settings</span>
+                </Link>
+              </DropdownMenuItem>
+            </DropdownMenuContent>
+          </DropdownMenu>
+        )}
+      </div>
 
       {/* Center: Name + Date */}
       <div className="flex items-baseline gap-2 text-center min-w-0">
